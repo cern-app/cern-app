d2c425580684ff86b1b7494d445accc8fc7d6783
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-04-17 14:33:20 +0200

    New BulletinFeedViewController

---
+++ Src/CustomTables/iPad/BulletinFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  BulletinFeedViewController.h
+//  CERN
+//
+//  Created by Timur Pocheptsov on 4/17/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "NewsFeedViewController.h"
+
+@interface BulletinFeedViewController : NewsFeedViewController
+
+@end
---
+++ Src/CustomTables/iPad/BulletinFeedViewController.mm
@@ -0,0 +1,38 @@
+//
+//  BulletinFeedViewController.m
+//  CERN
+//
+//  Created by Timur Pocheptsov on 4/17/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "BulletinFeedViewController.h"
+
+@interface BulletinFeedViewController ()
+
+@end
+
+@implementation BulletinFeedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
