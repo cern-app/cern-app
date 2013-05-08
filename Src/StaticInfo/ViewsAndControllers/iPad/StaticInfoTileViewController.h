253dd49b1051d1bad62adb8ba663f329f90c8034
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-05-08 09:47:53 +0200

    Add 'ViewControllers' directory for base classes.

---
+++ Src/StaticInfo/ViewsAndControllers/iPad/StaticInfoTileViewController.h
@@ -0,0 +1,13 @@
+//
+//  StaticInfoTileViewController.h
+//  CERN
+//
+//  Created by Timur Pocheptsov on 5/8/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaticInfoTileViewController : UIViewController
+
+@end
---
+++ Src/StaticInfo/ViewsAndControllers/iPad/StaticInfoTileViewController.mm
@@ -0,0 +1,38 @@
+//
+//  StaticInfoTileViewController.m
+//  CERN
+//
+//  Created by Timur Pocheptsov on 5/8/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "StaticInfoTileViewController.h"
+
+@interface StaticInfoTileViewController ()
+
+@end
+
+@implementation StaticInfoTileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
