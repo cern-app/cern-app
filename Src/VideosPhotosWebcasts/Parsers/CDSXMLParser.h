4ff7e5d8c93a81000376fa40dd33c63c80def6f7
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-10-02 17:52:07 +0200

    Add a new parser for CDS xmls - our "latest photos" and "latest videos".
    The old one is sort of .... sh.... ok.

---
+++ Src/VideosPhotosWebcasts/Parsers/CDSXMLParser.h
@@ -0,0 +1,28 @@
+//
+//  CDSXMLParser.h
+//  CERN
+//
+//  Created by Timur Pocheptsov on 10/2/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class CDSXMLParser;
+
+@protocol CDSXMLParserDelegate
+@required
+- (void) parser : (CDSXMLParser *) parser didParseRecord : (NSDictionary *) record;
+- (void) parserDidFinish : (CDSXMLParser *) parser;
+- (void) parser : (CDSXMLParser *) parser didFailWithError : (NSError *) error;
+@end
+
+@interface CDSXMLParser : NSObject<NSURLConnectionDataDelegate, NSXMLParserDelegate>
+
+@property (nonatomic) NSString *CDSUrl;
+@property (nonatomic, weak) NSObject<CDSXMLParserDelegate> *delegate;
+
+- (BOOL) start;
+- (void) stop;
+
+@end
---
+++ Src/VideosPhotosWebcasts/Parsers/CDSXMLParser.mm
@@ -0,0 +1,131 @@
+//
+//  CDSXMLParser.m
+//  CERN
+//
+//  Created by Timur Pocheptsov on 10/2/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import <cassert>
+
+#import "CDSXMLParser.h"
+
+//This parser will be created/executed by an operation object
+//(so, it's in a background thread). The parser itself uses
+//asynchronous connection == + 1 more thread (but according to
+//the documentation even synchronous connections create additional threads).
+//After xml data was downloaded, we parse it and pass the parsed data to
+//the delegate (it will be an operation object).
+
+@implementation CDSXMLParser {
+   NSURLConnection *connection;
+   NSMutableData *connectionData;
+   NSXMLParser *xmlParser;
+}
+
+@synthesize CDSUrl, delegate;
+
+//________________________________________________________________________________________
+- (BOOL) start
+{
+   assert(CDSUrl != nil && "start, CDSUrl is nil");
+   assert(connection == nil && xmlParser == nil && "start, parser was started already");
+
+
+   if (NSURL * const url = [NSURL URLWithString : CDSUrl]) {
+      NSURLRequest * const urlRequest = [[NSURLRequest alloc] initWithURL : url];
+      if (!urlRequest) {
+         NSLog(@"error<CDSXMLParser>: start - failed to create an url request");
+         return NO;
+      }
+
+      connectionData = [[NSMutableData alloc] init];
+      if (!(connection = [[NSURLConnection alloc] initWithRequest : urlRequest delegate : self])) {
+         connectionData = nil;
+         NSLog(@"error<CDSXMLParser>: start - failed to create a connection");
+         return NO;
+      }
+   } else {
+      NSLog(@"error<CDSXMLParser>: start - failed to create url");
+      return NO;
+   }
+   
+   return YES;
+}
+
+//________________________________________________________________________________________
+- (void) stop
+{
+   if (connection) {
+      assert(xmlParser == nil && "stop, xmlParser working while connection is active");
+      [connection cancel];
+      connection = nil;
+   } else if (xmlParser) {
+      assert(connection == nil && "stop, xmlParser working while connection is active");
+      [xmlParser abortParsing];
+   }
+}
+
+#pragma mark - NSURLConnectionDataDelegate
+
+//________________________________________________________________________________________
+- (void) connection : (NSURLConnection *) aConnection didReceiveData : (NSData *) data
+{
+#pragma unused(aConnection)
+
+   assert(data != nil && "connection:didReceieveData:, parameter 'data' is nil");
+   assert(connectionData != nil && "connection:didReceiveData:, connectionData is nil");
+   
+   [connectionData appendData : data];
+}
+
+//________________________________________________________________________________________
+- (void) connection : (NSURLConnection *) aConnection didFailWithError : (NSError *) error
+{
+#pragma unused(aConnection, error)
+
+   connectionData = nil;
+   connection = nil;
+   //TODO: inform our delegate!
+}
+
+//________________________________________________________________________________________
+- (void) connectionDidFinishLoading : (NSURLConnection *) aConnection
+{
+#pragma unused(aConnection)
+
+   //Verify data? Start parsing.
+}
+
+#pragma mark - NSXMLParserDelegate
+
+//________________________________________________________________________________________
+- (void) parserDidStartDocument : (NSXMLParser *) parser
+{
+
+}
+
+//________________________________________________________________________________________
+- (void) parser : (NSXMLParser *) parser didStartElement : (NSString *) elementName namespaceURI : (NSString *) namespaceURI
+         qualifiedName : (NSString *) qName attributes : (NSDictionary *) attributeDict
+{
+}
+
+//________________________________________________________________________________________
+- (void) parser : (NSXMLParser *) parser foundCharacters : (NSString *) string
+{
+}
+
+//________________________________________________________________________________________
+- (void) parser : (NSXMLParser *) parser didEndElement : (NSString *) elementName namespaceURI : (NSString *) namespaceURI
+         qualifiedName : (NSString *) qName
+{
+}
+
+//________________________________________________________________________________________
+- (void) parserDidEndDocument : (NSXMLParser *) parser
+{
+
+}
+
+@end
