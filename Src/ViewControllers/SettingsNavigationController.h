a6a9e66e830c5d8e84012cd4b3273de9b30a7c98
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-10-14 09:43:55 +0200

    "Settings" view/controller on iPhone (iOS 7) fix a navigation bar.

---
+++ Src/ViewControllers/SettingsNavigationController.h
@@ -0,0 +1,8 @@
+//I need this EXTREMELY STUPID class just to say: shouldAutorotate ... NO.
+//THANK YOU APPLE FOR THIS "BRILLIANT" DESIGN!
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsNavigationController : UINavigationController
+
+@end
---
+++ Src/ViewControllers/SettingsNavigationController.mm
@@ -0,0 +1,19 @@
+//
+//  SettingsNavigationController.m
+//  CERN
+//
+//  Created by Timur Pocheptsov on 14/10/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "SettingsNavigationController.h"
+
+@implementation SettingsNavigationController
+
+//________________________________________________________________________________________
+- (BOOL) shouldAutorotate
+{
+   return NO;
+}
+
+@end
