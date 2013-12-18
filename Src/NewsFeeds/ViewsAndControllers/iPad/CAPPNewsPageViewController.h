bb82ab152fcbc1b3a2eb2c978aac8081b6af2b33
Timur Pocheptsov <Timur.Pocheptsov@cern.ch>
2013-12-18 14:20:15 +0100

    New view controller for (at least now) feed items.

---
+++ Src/NewsFeeds/ViewsAndControllers/iPad/CAPPNewsPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAPPNewsPageViewController.h
+//  CERN
+//
+//  Created by Timur Pocheptsov on 18/12/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "CAPPTiledPageViewController.h"
+
+@interface CAPPNewsPageViewController : CAPPTiledPageViewController
+
+@end
---
+++ Src/NewsFeeds/ViewsAndControllers/iPad/CAPPNewsPageViewController.mm
@@ -0,0 +1,15 @@
+//
+//  CAPPNewsPageViewController.m
+//  CERN
+//
+//  Created by Timur Pocheptsov on 18/12/13.
+//  Copyright (c) 2013 CERN. All rights reserved.
+//
+
+#import "CAPPNewsPageViewController.h"
+
+@implementation CAPPNewsPageViewController
+
+
+
+@end
