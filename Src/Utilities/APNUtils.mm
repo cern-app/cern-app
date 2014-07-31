30fa01d25fcb5168e513222fddd570b54d6a2d91
Fons Rademakers <Fons.Rademakers@cern.ch>
2014-07-31 23:29:54 +0200

    fix 64-bit warning.

--- Src/Utilities/APNUtils.mm
+++ Src/Utilities/APNUtils.mm
@@ -15,7 +15,7 @@ NSString *Sha1Hash(NSString *src)
    NSData * const data = [src dataUsingEncoding : NSUTF8StringEncoding];
    uint8_t digest[CC_SHA1_DIGEST_LENGTH] = {};
 
-   CC_SHA1(data.bytes, data.length, digest);
+   CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
    NSMutableString * const output = [NSMutableString stringWithCapacity : CC_SHA1_DIGEST_LENGTH * 2];
    for (unsigned i = 0; i < CC_SHA1_DIGEST_LENGTH; ++i)
       [output appendFormat : @"%02x", digest[i]];
